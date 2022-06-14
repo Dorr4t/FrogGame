7e37a858921c45d039df68cdf48a0aae69a3f4df
Goga11112 <104033427+Goga11112@users.noreply.github.com>
2022-06-14 17:57:45 +0300

    Создал скрипт на изменение изображения при клике
    - Клик = новая картинка

---
+++ Assets/Scripts/Svap_Click.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SwapImage : MonoBehaviour
+{
+    public Sprite pic1;
+    public Sprite pic2;
+    private Image im;
+
+
+    void Start()
+    {
+        im = GetComponent<Image>();
+        im.sprite = pic1;
+    }
+
+    public void OnClick()
+    {
+        if (im.sprite == pic1)
+        {
+            im.sprite = pic2;
+            return;
+        }
+
+        if (im.sprite == pic2)
+        {
+            im.sprite = pic1;
+            return;
+        }
+    }
+}
